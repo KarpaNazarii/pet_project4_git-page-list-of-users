30e4c20b7bc68098f0cae58d8a81223dbfe55912
KarpaNazarii <nazar31004@ukr.net>
2022-10-24 16:25:57 +0300

    Renew img format

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Success = ({ count }) => {
   return (
     <div class="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src="%PUBLIC_URL%/assets/success.svg" alt="Success" />
       <h3>Successfully!</h3>
       <p>Invitations have been sent to {count} users</p>
       <button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
